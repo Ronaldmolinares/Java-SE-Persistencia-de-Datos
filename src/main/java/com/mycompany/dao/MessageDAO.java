b4a302f32adefd614dbf480a2b2c634526539b75
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-06-07 16:30:03 -0500

    Avanzando con la app

--- src/main/java/com/mycompany/dao/MessageDAO.java
+++ src/main/java/com/mycompany/dao/MessageDAO.java
@@ -4,28 +4,54 @@
  */
 package com.mycompany.dao;
 
+import com.mycompany.messages_app.ConnectionBD;
 import com.mycompany.model.Message;
+//paquete para conectarse con la base de datos
+import java.sql.Connection;
+//
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
  * @author Samir
  */
 public class MessageDAO {
-    
-    public void createMessageDB(Message message){
-        
+
+    public void createMessageDB(Message message) {
+        //Conectamos con nuestra base de datos
+        ConnectionBD db_connect = new ConnectionBD();
+
+        try(Connection con = db_connect.get_connection()) {
+            PreparedStatement ps = null;
+            try{
+                //Consulta para insertar los datos
+                String query = "INSERT INTO messages (message, autor_message) VALUES (?, ?)";
+                ps = con.prepareStatement(query);
+                //Preparamos los parametros que seran enviados a la base de datos
+                ps.setString(1, message.getMessage());
+                ps.setString(2, message.getAutor_message());
+                ps.executeUpdate();
+                System.out.println("");
+                
+            } catch (SQLException ex) {
+                System.out.println("ex");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
-    
-    public void readMessageDB(){
-        
+
+    public void readMessageDB() {
+
     }
-    
-    public void deleteMessageDB(int id_message){
-        
+
+    public void deleteMessageDB(int id_message) {
+
     }
-    
-    public void editMessageDB(Message message){
-        
+
+    public void editMessageDB(Message message) {
+
     }
-    
+
 }
--- src/main/java/com/mycompany/service/MessagesService.java
+++ src/main/java/com/mycompany/service/MessagesService.java
@@ -4,14 +4,34 @@
  */
 package com.mycompany.service;
 
+import com.mycompany.model.Message;
+import com.mycompany.dao.MessageDAO;
+import java.util.Scanner;
+
 /**
  *
  * @author Samir
  */
 public class MessagesService {
     
+    MessageDAO msgdao = new MessageDAO();
+    
     public void createMessage(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje: ");
+        String message = sc.nextLine();
         
+        System.out.println("Nombre del autor: ");
+        String name = sc.nextLine();
+        
+        Message register = new Message();
+        register.setMessage(message);
+        register.setAutor_message(name);
+        
+        //Enviamos el objeto creado a la capa DAO
+        msgdao.createMessageDB(register);
+        
+        System.out.println("Operacion exitosa");
     }
     
     public void ListMessage(){
